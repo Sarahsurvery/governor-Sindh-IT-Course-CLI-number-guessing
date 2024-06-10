ee04b5a3682636bcfb3170ba4c3180bde06ebe98
Sarahsurvery <163579811+Sarahsurvery@users.noreply.github.com>
2024-06-10 17:15:30 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+//1) computer will generate random number -Done
+
+//2) user input for guessing number
+
+//3) compare user input with computer generated number and show result
+
+const randomnumber = Math.floor(Math.random()* 6 + 1);
+console.log(randomnumber);
+
+ const answers = await inquirer.prompt([
+     {
+         name: "userguessednumber",
+         type: "number",
+         message: "please guess a number between 1-6:",
+
+}]);
+
+ if(answers.userguessednumber === randomnumber){
+     console.log("congratulation! you guessed right number");
+ }else{
+     console.log("you guessed wrong number");
+ }
+
